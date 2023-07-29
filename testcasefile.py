40313e138fe359c255fc2d952bae60586e91082a
Mahendra Tandon <mahendratandon.2k@gmail.com>
2023-07-29 13:25:48 +0530

    adding code from dev to release through automation# AUTOMATED

--- testcasefile.py
+++ testcasefile.py
@@ -1,28 +1,30 @@
-import os,csv,test_math
+import os,test_math
 import pandas as pd
 
 def findFailedStatus():
     found = False
     dataset = pd.read_csv('testreport.csv')
     for i in range(len(dataset)):
-        if(dataset == True):
-            found = dataset.loc[i][5] == 'failed'      
+        if(dataset.loc[i][6] == 'failed'):
+            found = True
     return found
 
 def mergeCodeToReleaseBranch():
-    # os.system('git add .')
-    # os.system('git commit -m "adding code from dev to release through automation# AUTOMATED"')  
-    # os.system('git push origin release"')
-    print("message added")
+    print('inside mergeCOdeToRelease')
+    os.system('git add .')
+    os.system('git commit -m "adding code from dev to release through automation# AUTOMATED"')  
+    os.system('git push origin release"')
 
 
 def runOsCommand():
     testpassed = findFailedStatus()
+    print('test-passed', testpassed)
     if (testpassed == True):
         mergeCodeToReleaseBranch()
     else:
         print("test case failed")   
-        
+
+runOsCommand()
 
 
 
