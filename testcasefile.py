22d1b2e2e7ac0d5b6b1916a73bfcbed2df0ff8a8
Mahendra Tandon <mahendratandon.2k@gmail.com>
2023-07-29 13:37:04 +0530

    AUTOMATED# Adding code from dev to release through automation

--- testcasefile.py
+++ testcasefile.py
@@ -10,13 +10,18 @@ def findFailedStatus():
     return found
 
 def mergeCodeToReleaseBranch():
-    print('inside mergeCOdeToRelease')
+    print('inside mergeCodeToRelease')
+    os.system('git checkout release')
+    os.system('git merge dev')
     os.system('git add .')
-    os.system('git commit -m "adding code from dev to release through automation# AUTOMATED"')  
+    os.system('git commit -m "AUTOMATED# Adding code from dev to release through automation"')  
     os.system('git push origin release"')
 
-
+def runAllTests():
+    os.system('python -m pytest')
+    
 def runOsCommand():
+    runAllTests()
     testpassed = findFailedStatus()
     print('test-passed', testpassed)
     if (testpassed == True):
